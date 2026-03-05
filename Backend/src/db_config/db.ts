b19ddb41f80bffac8b340dcc21405f328f4bd19d
r-mattasa <rubini.cbk@gmail.com>
2026-03-05 13:08:26 +0200

    updated

--- Backend/src/db_config/db.ts
+++ Backend/src/db_config/db.ts
@@ -1,24 +1,20 @@
 import pg from "pg";
 import dotenv from "dotenv";
-const { KP_PG_HOST, KP_PG_PORT, KP_PG_USERNAME, KP_PG_PASSWORD, KP_PG_DATABASE } = process.env;
-
 
 dotenv.config();
 
-
 export const pool = new pg.Pool({
-  host: process.env.KP_PG_HOST || "localhost",
-  port: Number(process.env.KP_PG_PORT) || 5433,
-  user: process.env.KP_PG_USERNAME || "postgres",
-  password: process.env.KP_PG_PASSWORD || "rubs123", // already a string
-  database: process.env.KP_PG_DATABASE || "KatevaPalvelut",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for most cloud DBs like Supabase
+  },
 });
 
 
 pool.connect()
   .then(() => console.log("Connected to PostgreSQL", process.env.KP_PG_DATABASE))
   .catch((error: any) => {
-    console.error("Database connectionnnn error:", error);
+    console.error("Database connection error:", error);
     // Some runtimes or TypeScript lib settings may not have AggregateError typed; detect aggregate-like shape safely
     const innerErrors = error && (error as any).errors;
     if (Array.isArray(innerErrors)) {
