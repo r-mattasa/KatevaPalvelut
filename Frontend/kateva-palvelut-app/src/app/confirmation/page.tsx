f52ae326ffdd412fee517318a163edcb234b04fd
r-mattasa <rubini.cbk@gmail.com>
2026-03-06 15:44:07 +0200

    move search params to another component

--- Frontend/kateva-palvelut-app/src/app/confirmation/page.tsx
+++ Frontend/kateva-palvelut-app/src/app/confirmation/page.tsx
@@ -5,30 +5,39 @@ import Grid from "@mui/material/Grid";
 import BookingConfirmationForm from "./booking-confirmation-form";
 import { useSearchParams } from 'next/navigation';
 import { BookingProps } from "../types/types";
+import { Suspense } from "react";
+
+const ReservationDetails = () => {
+  const params = useSearchParams();
+
+  const reservationProps: BookingProps = {
+    profileId: params.get('profileId'),
+    description: params.get('description'),
+    subServiceId: Number(params.get('subServiceId')),
+    slotStartTime: params.get('slotStartTime'),
+    slotEndTime: params.get('slotEndTime'),
+    slotDuration: Number(params.get('slotDuration')),
+    providerName: params.get('providerName'),
+    bookingDate: params.get('bookingDate'),
+    subServiceName: params.get('subServiceName')
+  };
+
+  return (
+    <Grid container spacing={2} alignItems="stretch">
+      <BookingConfirmationForm {...reservationProps} />
+    </Grid>
+  );
+};
 
 
 const ReservationConfirmationPage: React.FC = () => {
-   const params = useSearchParams(); 
-
-   const reservationProps: BookingProps = {
-         profileId: params.get('profileId'),
-         description: params.get('description'),
-         subServiceId: Number(params.get('subServiceId')),
-         slotStartTime: params.get('slotStartTime'),
-         slotEndTime: params.get('slotEndTime'),
-         slotDuration: Number(params.get('slotDuration')),
-         providerName: params.get('providerName'),
-         bookingDate:params.get('bookingDate'),
-         subServiceName:params.get('subServiceName')
-    };
   return (
-    <Box sx={{ py: 4, mt:6, boxShadow: 3, borderRadius: 0, backgroundColor:'#ffffff', textAlign:'center' }}>
-
-      <Grid container spacing={2} alignItems="stretch">
-       <BookingConfirmationForm {...reservationProps} />
-      </Grid>
+    <Box sx={{ py: 4, mt: 6, boxShadow: 3, borderRadius: 0, backgroundColor: '#ffffff', textAlign: 'center' }}>
+      <Suspense fallback={<div>Loading reservation details...</div>}>
+        <ReservationDetails />
+      </Suspense>
     </Box>
   );
-}
+};
 
 export default ReservationConfirmationPage;
